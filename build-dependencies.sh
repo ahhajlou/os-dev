3b275c1c75e069c055466e792889160b6b0178d4
Amirhossein Hajlou <hajlouamirhossein@gmail.com>
2025-11-07 19:24:08 +0330

    Fix the function

--- build-dependencies.sh
+++ build-dependencies.sh
@@ -5,23 +5,40 @@ set -ex
 export BINUTILS_VERSION=2.44
 export GCC_VERSION=14.2.0
 
-create_dir_or_remove_contents() {
+DEPENDENCIES_INSTALLED_FILE_PATH="${WORKSPACE_DIR}/dependencies/.dependencies-installed"
+
+ensure_clean_directory() {
     dir_name=$1
+
+    if [ -z "$dir_name" ]; then
+        echo "ensure_clean_directory: directory path must be provided" >&2
+        exit 1
+    fi
+
+    case "$dir_name" in
+        "/"|"."|".."|"")
+            echo "ensure_clean_directory: refusing to operate on '$dir_name'" >&2
+            exit 1
+            ;;
+    esac
+
     if [ -d "$dir_name" ]; then
-        rm -rf "$dir_name"/*
+        find "$dir_name" -mindepth 1 -maxdepth 1 -exec rm -rf {} +
     else
         mkdir -p "$dir_name"
     fi
+
+    mkdir -p "$dir_name"
 }
 
-if [ -f "${WORKSPACE_DIR}/.dependencies-installed" ]; then
+if [ -f "${DEPENDENCIES_INSTALLED_FILE_PATH}" ]; then
     exit 0
 fi
 
 echo -e "\033[1;32mStarting to build dependencies\033[0m"
 
-create_dir_or_remove_contents "$PREFIX"
-create_dir_or_remove_contents "$SRC_DIR"
+ensure_clean_directory "$PREFIX"
+ensure_clean_directory "$SRC_DIR"
 
 echo -e "\033[1;32mDownloading files\033[0m"
 
@@ -62,6 +79,6 @@ ${SRC_DIR}/gcc-${GCC_VERSION}/configure \
       make install-target-libgcc && \
       make install-target-libstdc++-v3
 
-touch "${WORKSPACE_DIR}/.dependencies-installed"
+touch "${DEPENDENCIES_INSTALLED_FILE_PATH}"
 
 exit 0
