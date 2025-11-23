4f94e35989c7c303ac3ddfb02ba18fbbdb4cb4a4
Amirhossein Hajlou <hajlouamirhossein@gmail.com>
2025-11-23 14:24:13 +0330

    Add kernel source files

---
+++ kernel/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+## You can then assemble boot.s using:
+i686-elf-as boot.s -o boot.o
+
+## Compile using:
+i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+
+## You can then link your kernel using: 
+i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
